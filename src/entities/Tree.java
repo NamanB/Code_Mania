048b7cf04c4852bbc6a5bd822e2ac58539fd6fca
Sasha2424 <sashaivanov2424@gmail.com>
2017-12-16 19:17:09 -0800

    added tree entity

---
+++ src/entities/Tree.java
@@ -0,0 +1,29 @@
+package  entities;
+
+import java.awt.Graphics2D;
+
+import entities.Entity;
+import main.GameWindow;
+
+
+public class Tree extends Entity {
+	
+	private static final int WIDTH = 40;
+	private static final int HEIGHT = 40;
+
+	public Tree(double X, double Y) {
+		super(X, Y, new int[]{0}, new int[]{0});
+		this.setHeight(WIDTH);
+		this.setWidth(HEIGHT);
+		this.type = "player";
+		this.name = "JOE"; //TODO implement player name input
+	}
+	
+	
+	//TODO for multiple textures
+//	public void draw(GameWindow w, Graphics2D g, Player player) {
+//		g.drawImage(texture[0], (int) (x - player.getX()), (int) (y + player.getY()), width, height, null);
+//	}
+	
+
+}
