b785d8d66bcc5cc62f136baacd79a1323ac36e05
Sasha2424 <sashaivanov2424@gmail.com>
2017-12-16 18:40:36 -0800

    added commented out draw method in player for future animation

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,18 +1,29 @@
 package player;
 
+import java.awt.Graphics2D;
+
 import entities.Entity;
+import main.GameWindow;
+
 
 public class Player extends Entity {
-
+	
 	private static final int WIDTH = 40;
 	private static final int HEIGHT = 40;
 
 	public Player(double X, double Y, int[] x2, int[] y2) {
-		super(X, Y, x2, y2);
+		super(X, Y, new int[]{0}, new int[]{0});
 		this.setHeight(WIDTH);
 		this.setWidth(HEIGHT);
 		this.type = "player";
-		this.name = "JOE"; // TODO implement player name input
+		this.name = "JOE"; //TODO implement player name input
 	}
+	
+	
+	//TODO for multiple textures
+//	public void draw(GameWindow w, Graphics2D g, Player player) {
+//		g.drawImage(texture[0], (int) (x - player.getX()), (int) (y + player.getY()), width, height, null);
+//	}
+	
 
 }
