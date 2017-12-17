12e282b86388210f44f12e2cfee3dc4bd3a39568
Sasha2424 <sashaivanov2424@gmail.com>
2017-12-16 18:19:12 -0800

    added player size and setDims() method

--- src/player/Player.java
+++ src/player/Player.java
@@ -2,11 +2,21 @@ package player;
 
 import entities.Entity;
 
-public class Player extends Entity{
+public class Player extends Entity {
+	
+	private static final int WIDTH = 40;
+	private static final int HEIGHT = 40;
 
 	public Player(double X, double Y, int[] x2, int[] y2) {
 		super(X, Y, x2, y2);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setDims() {
+		this.setHeight(WIDTH);
+		this.setWidth(HEIGHT);
+
 	}
 
 }
